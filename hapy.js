a5dfc3f7787409c3ffc7184e5e1a98f40c44487a
Vaisakhkm2625 <68694876+Vaisakhkm2625@users.noreply.github.com>
2020-11-08 11:17:52 +0530

    Update hapy.js

--- hapy.js
+++ hapy.js
@@ -1,17 +1,65 @@
 
+
+
 const heart = [];
 let a = 0;
 let t =0,dt;
 var r;
 
+
+
+class fallball{
+  xpos;
+  ypos;
+  yvel;
+  yacc;
+  sise;
+  constructor(){
+    
+    this.setu();
+  } 
+  setu(){
+    this.xpos = random(-width/2,width/2);
+    this.ypos = random(-height/2,0);
+    this.yvel = 0;
+    this.yacc = 0.1;
+    this.sise = random(10,100);
+  }
+  show(){
+    fill(color(100,100,100));
+    circle(this.xpos,this.ypos,this.sise);
+  }
+  nextstep(){
+    this.yvel +=this.yacc; 
+    this.ypos +=this.yvel;
+this.show();
+    if(height/2<(this.ypos+this.sise)){
+      this.setu();
+    } 
+  }
+
+}
+
+
+let balls = [];
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 if(width<height){ 
      r = width/40;
-    dt = 0.5}
+    dt = 0.2}
    else{
      r = height/40
-    dt =0.8}
+    dt =0.32}
+
+    for(var i=0;i<4;i++){
+      balls[i] = new fallball();
+    }
+
+
+
 }
 
 
@@ -19,6 +67,12 @@ function draw() {
   background(color(100,100,255));
   translate(width/2, height/2);
 
+
+  for(var i=0;i<4;i++){
+    balls[i].nextstep();
+  }
+
+  
   stroke(255);
   strokeWeight(2);
   fill(150, 0, 100);
@@ -36,13 +90,17 @@ function draw() {
   fill(255);
   textAlign(CENTER);
   textSize(t);
-  text("happy birthday Dennis",0,0);
+  text("happy birthday Arun",0,0);
+  textSize(10);
+
+
+  
+  
  
-  // So that it stops
   if (a > TWO_PI) {
     noLoop();
   }
   t += dt;
-  a += 0.1;
+  a += 0.04;
  // delay(100);
 }
